57d1d5e34f2e2c96c83d382959b864fecfde4fa3
chick <chick@qrhino.com>
2020-11-02 08:41:51 -0800

    Added conditional step running

--- src/release_scripts/build_masters.py
+++ src/release_scripts/build_masters.py
@@ -1,11 +1,11 @@
-#
+"""Updates chisel-release master branch to latest master hash"""
 
 import os
 import sys
 
-print(f"cwd is {os.getcwd()}")
-from release_scripts.git_utils import tools
+from release_scripts.git_utils.tools import Tools
 
+print(f"chisel-release directory is {os.getcwd()}")
 
 if len(sys.argv) < 2:
     print("Usage: $0 <release repo>")
@@ -14,20 +14,21 @@ if len(sys.argv) < 2:
 current_dir = sys.argv[1]
 os.chdir(current_dir)
 
-tools.check_release_dir()
+tools = Tools("build_masters")
+tools.set_start_step(11)
 
-tools.checkout_branch("master")
+tools.checkout_branch(1, "master")
 
-tools.run_pull()
+exit(1)
 
-tools.run_submodule_update_recursive()
+tools.run_pull(2)
 
-tools.run_make_pull()
+tools.run_submodule_update_recursive(3)
 
-tools.run_make_clean()
+tools.run_make_pull(4)
 
-tools.run_make_install()
+tools.run_make_clean_install(5)
 
-#tools.run_make_test()
+tools.run_make_test(6)
 
 
--- src/release_scripts/git_utils/tools.py
+++ src/release_scripts/git_utils/tools.py
@@ -2,89 +2,181 @@
 
 import os
 import subprocess
+import re
 
 
-def check_release_dir():
-    command_result = subprocess.run(["git", "remote", "-v"], text=True, capture_output=True)
-    if command_result.returncode != 0:
-        print("You appear to be in the wrong directory")
-        print(f"{os.getcwd()} does not appear to be a git repo")
+def run_this_step(step_function):
+    """
+    Conditionally runs a step
+    """
+    def wrapper(*args, **kwargs):
+        print(f"arg1 {args[0]}")
+        tool_object = args[0]
+        step_number = args[1]
+        start_step = getattr(tool_object, 'get_start_step')(tool_object)
+        stop_step = getattr(tool_object, 'get_stop_step')(tool_object)
+
+        if start_step <= step_number <= stop_step:
+            print(f"running step {step_number}")
+            print(f"step {step_function(*args, **kwargs)}")
+        else:
+            print(f"skipping step {step_number}")
+        # print(f"step, start, stop {step_number} {start_step} { stop_step}")
+        # print(f"object {dir(tool_object)}")
+        # print(f"object {getattr(tool_object, 'get_start_step')(tool_object)}")
+        # print(f"step {step_function(*args, **kwargs)}")
+        # print(step_function)
+
         exit(1)
 
-
-def checkout_branch(branch_name):
-    """checkout specified branch"""
-
-    command_result = subprocess.run(["git", "checkout", branch_name], text=True, capture_output=True)
-    if command_result.returncode != 0:
-        print(f"git checkout {branch_name} failed")
-        exit(1)
-
-    print(f"Now on branch {branch_name}")
+    return wrapper
 
 
-def run_pull():
-    """runs git pull"""
+class Tools:
+    def __init__(self, task_name):
+        self.task_name = task_name
+        self.log_name = f"log_{task_name}"
+        self.check_release_dir()
 
-    command_result = subprocess.run(["git", "pull"], text=True, capture_output=True)
-    if command_result.returncode != 0:
-        print(f"git pull somehow failed")
-        exit(1)
+        self.white_space = re.compile('\s')
 
-    print(f"git pull complete")
+        if not os.path.exists(self.log_name):
+            os.mkdir(self.log_name)
+        elif not os.path.isdir(self.log_name):
+            print(f"Error: {self.log_name} exists but is not a directory")
+            exit(1)
 
+        self.start_step, self.stop_step = -1, 1000
 
-def run_submodule_update_recursive():
-    """run git submodule update --init --recursive"""
+    def set_start_step(self, start_step):
+        self.start_step = start_step
 
-    command_result = subprocess.run(["git", "submodule", "update", "--init", "--recursive"], text=True,
-                                    capture_output=True)
-    if command_result.returncode != 0:
-        print(f"git submodule update recursive failed")
-        exit(1)
+    def set_stop_step(self, stop_step):
+        self.stop_step = stop_step
 
-    print(f"git submodule update recursive complete")
+    def get_start_step(self, start_step):
+        return self.start_step
 
+    def get_stop_step(self, stop_step):
+        return self.stop_step
 
-def run_make_pull():
-    """run make pull"""
+    def check_step(self, step_number: int) -> bool:
+        step_number >= self.start_step and step_number <= self.stop_step
 
-    command_result = subprocess.run(["make", "pull"], text=True, capture_output=True)
-    if command_result.returncode != 0:
-        print(f"make pull failed")
-        exit(1)
+    def check_release_dir(self):
+        """Looks to see that target is a clone of chisel-release"""
+        command_result = subprocess.run(["git", "remote", "-v"], text=True, capture_output=True)
+        if command_result.returncode != 0:
+            print("You appear to be in the wrong directory")
+            print(f"{os.getcwd()} does not appear to be a git repo")
+            exit(1)
 
-    print(f"make pull complete")
+    @run_this_step
+    def checkout_branch(self, step_number, branch_name: str) -> None:
+        """checkout specified branch"""
 
+        function_name = "checkout_branch"
+        log_name = self.step_log_name(step_number, function_name)
 
-def run_make_clean():
-    """run make pull"""
+        command_result = subprocess.run(
+            f"git checkout {branch_name} &> {log_name}",
+            shell=True,
+            capture_output=False)
+        if command_result.returncode != 0:
+            print(f"git checkout {branch_name} failed, see {log_name} for details")
+            exit(1)
 
-    command_result = subprocess.run(["make", "clean"], text=True, capture_output=True)
-    if command_result.returncode != 0:
-        print(f"make clean failed")
-        exit(1)
+        print(f"Now on branch {branch_name}")
 
-    print(f"make clean complete")
+    def run_pull(self, step_number: int) -> None:
+        """runs git pull"""
 
+        function_name = "run_pull"
+        log_name = self.step_log_name(step_number, function_name)
 
-def run_make_install():
-    """run make install"""
+        command_result = subprocess.run(
+            f"git pull &> {log_name}",
+            shell=True,
+            capture_output=False)
+        if command_result.returncode != 0:
+            print(f"git pull failed, see {log_name} for details")
+            exit(1)
 
-    command_result = subprocess.run(["make", "-j8", "install"], text=True, capture_output=True)
-    if command_result.returncode != 0:
-        print(f"make install failed")
-        exit(1)
+        print(f"git pull complete")
 
-    print(f"make install complete")
+    def run_submodule_update_recursive(self, step_number):
+        """run git submodule update --init --recursive"""
 
+        function_name = "run_submodule_update_recursive"
+        log_name = self.step_log_name(step_number, function_name)
 
-def run_make_test():
-    """run make test"""
+        command_result = subprocess.run(
+            f"git submodule update --init --recursive &> {log_name}",
+            shell=True,
+            capture_output=False)
 
-    command_result = subprocess.run(["make", "-j8", "test"]) # , text=True, capture_output=True)
-    if command_result.returncode != 0:
-        print(f"make install failed")
-        exit(1)
+        if command_result.returncode != 0:
+            print(f"git submodule update recursive failed, see {log_name} for details")
+            exit(1)
 
-    print(f"make install complete")
+        print(f"git submodule update --init --recursive complete")
+
+    def run_make_pull(self, step_number):
+        """run make pull"""
+
+        function_name = "make_pull"
+        log_name = self.step_log_name(step_number, function_name)
+
+        command_result = subprocess.run(
+            f"make pull &> {log_name}",
+            shell=True,
+            capture_output=False)
+        if command_result.returncode != 0:
+            print(f"make pull failed, see {log_name} for details")
+            exit(1)
+
+        print(f"make pull complete")
+
+    def run_make_clean_install(self, step_number):
+        """run make clean install"""
+
+        function_name = "run_make_clean_install"
+        log_name = self.step_log_name(step_number, function_name)
+
+        command_result = subprocess.run(
+            f"make -j8 -f Makefile clean pull &> {log_name}",
+            shell=True,
+            capture_output=False)
+
+        if command_result.returncode != 0:
+            print(f"make -j8 -f Makefile clean install failed, see {log_name} for details")
+            exit(1)
+
+        print(f"make clean install complete")
+
+    def run_make_test(self, step_number):
+        """run make test"""
+
+        function_name = "run_make_test"
+        log_name = self.step_log_name(step_number, function_name)
+
+        command_result = subprocess.run(
+            f"make -j8 -f Makefile test &> {log_name}",
+            shell=True,
+            capture_output=False)
+
+        if command_result.returncode != 0:
+            print(f"make -j8 -f Makefile clean install failed, see {log_name} for details")
+            exit(1)
+
+        print(f"make test complete")
+
+    def step_log_name(self, step_number: int, step_name) -> str:
+        """create the correct name for a log file"""
+
+        log_file_name = f"{self.log_name}/step_{step_number:03d}_{step_name}"
+        if self.white_space.search(log_file_name):
+            print(f"Error: {step_number} {step_name} generated log name '{log_file_name} contains white space")
+            exit(1)
+
+        return log_file_name
