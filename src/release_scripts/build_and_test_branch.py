3a4f0c5ff81fe4ba9b591d89d8e428dee1e714b4
chick <chick@qrhino.com>
2021-01-28 10:17:01 -0800

    Some minor updates make-dot-x-dash-release.py is to make the Z.Y.x and Z.Y-release branches in chisel-release and it's submodules after a new release has been done

--- src/release_scripts/build_and_test_branch.py
+++ src/release_scripts/build_and_test_branch.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-import getopt
+from argparse import ArgumentParser
 
 from release_scripts.git_utils.tools import Tools
 from release_scripts.git_utils.step_counter import StepCounter
@@ -20,70 +20,63 @@ def usage():
 
 def main():
     try:
-        opts, args = getopt.getopt(
-            sys.argv[1:],
-            "lhr:m:s:e:",
-            ["help", "repo=", "release=", "start-step=", "stop-step=", "list-only"]
-        )
-    except getopt.GetoptError as err:
-        print(err)
-        usage()
+        parser = ArgumentParser()
+        parser.add_argument('-r', '--release', dest='release_dir', action='store',
+                            help='a directory which is a clone of chisel-release', required=True)
+        parser.add_argument('-br', '--branch', dest='branch', action='store',
+                            help='major number of snapshots being published', required=True)
+
+        parser.add_argument('-b', '--start-step', dest='start_step', type=int, action='store',
+                            help='command step to start on',
+                            default=1)
+        parser.add_argument('-e', '--stop-step', dest='stop_step', type=int, action='store',
+                            help='command step to end on',
+                            default=10000)
+        parser.add_argument('-l', '--list-only', dest='list_only', action='store_true',
+                            help='list command step, do not execute', default=False)
+
+        args = parser.parse_args()
+
+        release_dir = args.release_dir
+        branch = args.branch
+        start_step = args.start_step
+        stop_step = args.stop_step
+        list_only = args.list_only
+        counter = StepCounter()
+
+        tools = Tools("build_and_test_branch", release_dir)
+
+        if not list_only:
+            if release_dir == "" or branch == "":
+                print(f"Error: both --repo and --release must be specified to run this script")
+                usage()
+                exit(1)
+            else:
+                print(f"chisel-release directory is {os.getcwd()}")
+                print(f"release specified is {branch}")
+        else:
+            print(f"These are the steps to be executed for the {tools.task_name} script")
+
+        tools.set_start_step(start_step)
+        tools.set_stop_step(stop_step)
+        tools.set_list_only(list_only)
+
+        tools.checkout_branch(counter.next_step(), branch)
+
+        tools.git_pull(counter.next_step())
+
+        tools.run_submodule_update_recursive(counter.next_step())
+
+        tools.run_make_pull(counter.next_step())
+
+        tools.run_make_clean_install(counter.next_step())
+
+        tools.run_make_test(counter.next_step())
+
+    except Exception as e:
+        print(e)
         sys.exit(2)
 
-    release_dir = ""
-    start_step = -1
-    stop_step = 1000
-    list_only = False
-    release_version = ""
-    counter = StepCounter()
-
-    for option, value in opts:
-        if option in ("--repo", "-r"):
-            release_dir = value
-        elif option in ("--release", "-m"):
-            release_version = f"{value}.x"
-        elif option in ("--start-step", "-s"):
-            start_step = int(value)
-        elif option in ("--stop-step", "-e"):
-            stop_step = int(value)
-        elif option in ("--list-only", "-l"):
-            list_only = True
-        elif option in ("--help", "-h"):
-            usage()
-            exit(1)
-        else:
-            print(f"Unhandled command line option: {option}")
-            usage()
-            assert False
-
-    tools = Tools("build_and_test_branch", release_dir)
-
-    if not list_only:
-        if release_dir == "" or release_version == "":
-            print(f"Error: both --repo and --release must be specified to run this script")
-            usage()
-            exit(1)
-        else:
-            print(f"chisel-release directory is {os.getcwd()}")
-            print(f"release specified is {release_version}")
-    else:
-        print(f"These are the steps to be executed for the {tools.task_name} script")
-
-    tools.set_start_step(start_step)
-    tools.set_stop_step(stop_step)
-    tools.set_list_only(list_only)
-
-    tools.checkout_branch(counter.next_step(), release_version)
-
-    tools.git_pull(counter.next_step())
-
-    tools.run_submodule_update_recursive(counter.next_step())
-
-    tools.run_make_pull(counter.next_step())
-
-    tools.run_make_clean_install(counter.next_step())
-
-    tools.run_make_test(counter.next_step())
 
 if __name__ == "__main__":
     main()
---
+++ src/release_scripts/make-dot-x-dash-release.py
@@ -0,0 +1,61 @@
+"""
+After a new major release has been made you can use this script to create the
+`Z.Y.x` and `Z.Y-release` branches.
+TODO: This is still a WIP, needs more docs and error checks
+"""
+import os
+import subprocess
+
+import yaml
+
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+from version.Version import CNVersion
+
+
+def load_versions_file(config_file_name: str) -> dict:
+    config_input = open(config_file_name, 'r', encoding="utf-8")
+    version_configs = yaml.load(config_input, Loader=Loader)
+    config_input.close()
+    internal_configs = {}
+    for modulePath, versions in version_configs['versions'].items():
+        internal_configs[modulePath] = {}
+        internal_configs[modulePath]['packageName'] = versions['packageName']
+        internal_configs[modulePath]['version'] = CNVersion(aString=versions['version'])
+
+    return internal_configs
+
+
+versions_from_yaml = load_versions_file("../../../masters/version.yml")
+
+repo_versions = {}
+for key in versions_from_yaml.keys():
+    repo_versions[key] = versions_from_yaml[key]['version']
+
+for repo, version in repo_versions.items():
+    print(f"repo: {repo} -> {version}")
+
+save_dir = os.getcwd()
+
+for a, b in repo_versions.items():
+    dot_x = f"{b}.x"
+    dash_release = f"{b}-release"
+    print(f"{a} -> ({dot_x}, {dash_release})")
+    os.chdir(f"../masters/{a}")
+    result = subprocess.run(["git branch --show-current"], shell=True, capture_output=True)
+    branch = result.stdout.decode("utf-8")[:-1]
+    print(f"current branch {branch}")
+    if result.returncode == 0 and branch == "master":
+        print(f"creating branch {dot_x}")
+        subprocess.run(f"git checkout -b {dot_x}", shell=True)
+        subprocess.run(f"git push -u origin {dot_x}", shell=True)
+
+        print(f"creating branch {dash_release}")
+        subprocess.run(f"git checkout -b {dash_release}", shell=True)
+        subprocess.run(f"git push -u origin {dash_release}", shell=True)
+    else:
+        print(f"branch {dot_x} already exists")
+
+    os.chdir(save_dir)
