ac87ed052638ac95b3114b13063697cbd4c7d6c5
Jack Koenig <koenig@sifive.com>
2021-09-01 11:30:01 -0700

    WIP cont.

--- publish/publish_new_release_in_docker.py
+++ publish/publish_new_release_in_docker.py
@@ -2,6 +2,7 @@
 """publishes a new release, either major or minor"""
 
 import os
+from os.path import expandvars
 import sys
 from argparse import ArgumentParser
 import subprocess
@@ -15,14 +16,39 @@ def gitConfigGet(value):
     value = proc.stdout.decode().strip()
     return value
 
+def platform():
+    if sys.platform == "darwin":
+        return "macos"
+    elif sys.platform.startswith("linux"):
+        return "linux"
+    else:
+        raise Exception(f"Unsupported platform {sys.platform}")
+
+
+def platformSpecific(key):
+    lookup = {
+        "macos": {
+            "sshagent": "/run/host-services/ssh-auth.sock"
+        },
+        "linux": {
+            "sshagent": "$SSH_AUTH_SOCK" # TODO may need to do env lookup
+        }
+    }
+    return lookup[platform()][key]
+
+
+def sshAgent():
+    return platformSpecific("sshagent")
 
 def makeParser():
     # TODO expose and propagate options from 'publish_new_release.py'
     parser = ArgumentParser()
     parser.add_argument("-e", "--email", action="store", default=gitConfigGet("user.email"),
-                        help="Git config user.email, defaults to `git config --get user.email")
+                        help="Git config user.email, defaults to 'git config --get user.email'")
     parser.add_argument("-n", "--name", action="store", default=gitConfigGet("user.name"),
-                        help="Git config user.name, defaults to `git config --get user.name")
+                        help="Git config user.name, defaults to 'git config --get user.name'")
+    parser.add_argument("--ssh-agent", action="store", default=sshAgent(),
+                        help=f"Git config user.name, defaults to '{sshAgent()}'")
     return parser
 
 
@@ -30,6 +56,9 @@ def main():
     parser = makeParser()
     args = parser.parse_args()
     print(args)
+
+    container_home = "/root"
+    host_home = expandvars("$HOME")
 
     # Step 1 - Build Docker Image
     # TODO actually do this
@@ -46,17 +75,23 @@ def main():
         f"git config --global user.email {args.email}",
         f"git config --global user.name {args.name}",
         f"git config -l",
-        "hostname"
+        "hostname",
+        f"git clone git@github.com:chipsalliance/treadle.git"
     ]
     joined = "; ".join(lines)
     script = f"bash -c '{joined}'"
-    cmd = ["docker", "run", "--rm", "-it", "chiselrelease:latest", "bash", "-c", joined]
+    base_cmd = ["docker", "run", "--rm", "-it"]
+    # SSH Agent forwarding
+    ssh_agent = ["-v", f"{args.ssh_agent}:/ssh-agent", "-e", "SSH_AUTH_SOCK=/ssh-agent"]
+    # Known hosts mapping
+    known_hosts = ["-v", f"{host_home}/.ssh/known_hosts:{container_home}/.ssh/known_hosts"]
+    cmd =  base_cmd + ssh_agent + known_hosts + [ "chiselrelease:latest", "bash", "-c", joined]
     proc1 = subprocess.run(["echo", "$PATH"], shell=True)
     print(proc1)
     print(cmd)
-    proc = subprocess.run(cmd, capture_output=True)
-    print(proc.stdout.decode())
-    print(proc.stderr.decode())
+    proc = subprocess.run(cmd) #, capture_output=True)
+    #print(proc.stdout.decode())
+    #print(proc.stderr.decode())
 
 
 if __name__ == "__main__":
